ff7b4414979b7f5d5b9f26f38a1f67bc091ce935
konapun <bremen.braun@gmail.com>
2015-05-02 17:55:26 -0500

    Added routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,10 @@
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
-
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
+  var defaultRoute = 0;
   app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+    this.route = this.route || defaultRoute;
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
