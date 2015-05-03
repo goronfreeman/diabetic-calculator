31beb9bb126e0852cd302d36cdf7caf3cc631c42
konapun <bremen.braun@gmail.com>
2015-05-02 19:29:55 -0500

    Autoclose drawer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,16 @@
   'use strict';
 
   var app = document.querySelector('#app');
+
   var defaultRoute = 'calculator';
   app.addEventListener('template-bound', function() {
     this.route = this.route || defaultRoute;
   });
+
+  app.menuItemSelected = function(e, detail, sender) { // close drawer when a drawer item is selected
+    if (detail.isSelected) {
+      drawer.closeDrawer();
+    }
+  };
 
 })(wrap(document));
